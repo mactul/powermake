3e29738ff90dc1544ae3e81947dd9e1662c314d4
mactul <mactulgames@gmail.com>
2024-07-03 15:32:46 +0200

    change the current working directory to be the location of the makefile

--- blazymake/__init__.py
+++ blazymake/__init__.py
@@ -1,6 +1,7 @@
 import os
 import glob
 import fnmatch
+import __main__ as __makefile__
 from threading import Lock
 from concurrent.futures import ThreadPoolExecutor
 
@@ -8,6 +9,8 @@ from .config import Config
 from .operation import Operation, needs_update
 
 NB_JOBS = 8
+
+os.chdir(os.path.dirname(os.path.realpath(__makefile__.__file__)))
 
 
 def get_files(*patterns: str) -> list:
