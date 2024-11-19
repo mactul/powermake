108281ba8ff6304fc3fbc97520c2df0d22fa3aac
mactul <mactulgames@gmail.com>
2024-11-19 18:29:38 +0100

    improve tests

--- tests/multiplatform/makefile.py
+++ tests/multiplatform/makefile.py
@@ -1,7 +1,9 @@
+import os
 import powermake
 import subprocess
 
 prog_test = True
+assert_cc = None
 
 def on_build(config: powermake.Config):
     config.add_c_cpp_as_asm_flags("-Weverything")
@@ -12,9 +14,13 @@ def on_build(config: powermake.Config):
 
     files = powermake.get_files("*.c", "*.cpp")
 
+
     objects = powermake.compile_files(config, files)
 
     exe = powermake.link_files(config, objects)
+
+    if assert_cc is not None:
+        assert os.path.basename(assert_cc) == os.path.basename(config.c_compiler.path)
 
     if prog_test:
         print("testing generated program")
@@ -25,10 +31,13 @@ def on_build(config: powermake.Config):
 
 parser = powermake.ArgumentParser()
 parser.add_argument("--no-prog-test", action="store_true")
+parser.add_argument("--assert-cc", metavar="CC")
 
 args_parsed = parser.parse_args()
 
 if args_parsed.no_prog_test:
     prog_test = False
+
+assert_cc = args_parsed.assert_cc
 
 powermake.run("test", build_callback=on_build, args_parsed=args_parsed)
--- tests/tests.sh
+++ tests/tests.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -x
+
 function failure
 {
     echo "Tests failed"
@@ -19,15 +21,25 @@ then
 fi
 
 echo "checking multiplatform makefile with default, clang and MinGW toolchains"
+
 python3 ./multiplatform/makefile.py -rv || failure
-CC=clang python3 ./multiplatform/makefile.py -rv || failure
+
+CC=clang python3 ./multiplatform/makefile.py -rv --assert-cc="clang" || failure
+
+CC=aarch64-linux-gnu-gcc python3 ./multiplatform/makefile.py -rvd --no-prog-test --assert-cc="aarch64-linux-gnu-gcc" || failure
+
+python3 ./multiplatform/makefile.py -rv --l ./linux_config1.json --assert-cc="clang" || failure
+python3 ./multiplatform/makefile.py -rv --l ./linux_config2.json --no-prog-test --assert-cc="aarch64-linux-gnu-gcc" || failure
 
 if [[ $# -ne 1 && $1 != "weak" ]]
 then
-CC=x86_64-w64-mingw32-gcc python3 ./multiplatform/makefile.py -rv || failure
+NO_PROG_TEST=
 else
-CC=x86_64-w64-mingw32-gcc python3 ./multiplatform/makefile.py -rv --no-prog-test || failure
+NO_PROG_TEST="--no-prog-test"
 fi
+CC=x86_64-w64-mingw32-gcc python3 ./multiplatform/makefile.py -rv --assert-cc="x86_64-w64-mingw32-gcc" $NO_PROG_TEST || failure
+python3 ./multiplatform/makefile.py -rv -l ./windows_config1.json --assert-cc="x86_64-w64-mingw32-gcc" $NO_PROG_TEST || failure
+python3 ./multiplatform/makefile.py -rv -l ./windows_config2.json --assert-cc="i686-w64-mingw32-gcc" $NO_PROG_TEST || failure
 
 echo "testing lib compilation and link accross powermake makefiles, in release and in debug"
 python3 ./lib_depend/makefile.py -rv || failure
