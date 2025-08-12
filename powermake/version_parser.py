f11aa69e0ed2ad5fecc22ba82e0ff1363975653d
mactul <mactulgames@gmail.com>
2025-08-12 15:57:42 +0200

    improve version parser

--- powermake/version_parser.py
+++ powermake/version_parser.py
@@ -103,74 +103,83 @@ class Version():
 
 
 def remove_version_frills(string: str) -> str:
+    separators = ('.', '-', '+', ':', '!', '_')
     output = ""
     i = 0
     marker = 0
-    while i < len(string) and (string[i].isdigit() or string[i] in ('*', ':', '!')):
-        if string[i] in (':', '!'):
-            marker = i
-        i += 1
-    while i < len(string) and (string[i].isdigit() or string[i] in ('*', '.')):
-        if string[i] == '.':
-            marker = i
-        i += 1
-    output = string[0:marker]
     while i < len(string):
-        if string[i] in ('.', '-', '+'):
-            if re.fullmatch("([0-9]+|\\*)|((alpha|beta|a|b|rc|post|p|develop|dev|d)([0-9]+|\\*)?)", string[marker+1:i]):
-                output += string[marker:i]
+        if string[i] in separators:
+            if len(output) == 0 and string[marker+1 if marker > 0 else 0: i] in ("pre", "prerelease") or re.fullmatch(f"{'v?(pre|prerelease)?' if len(output) == 0 else ''}([0-9]+|\\*)|((alpha|beta|a|b|rc|post|p|develop|dev|d)([0-9]+|\\*)?)", string[marker+1 if string[marker] in separators else 0: i], re.IGNORECASE):
+                if len(output) == 0 and string[marker] in separators:
+                    output += string[marker+1:i]
+                else:
+                    output += string[marker:i]
             marker = i
         i += 1
-    if re.fullmatch("([0-9]+|\\*)|((alpha|beta|a|b|rc|post|p|develop|dev|d)([0-9]+|\\*)?)", string[marker+1:i]):
-        output += string[marker:i]
+    if i == 0:
+        return ""
+    if len(output) == 0 and string[marker+1 if marker > 0 else 0: i] in ("pre", "prerelease") or re.fullmatch(f"{'v?(pre|prerelease)?' if len(output) == 0 else ''}([0-9]+|\\*)|((alpha|beta|a|b|rc|post|p|develop|dev|d)([0-9]+|\\*)?)", string[marker+1 if string[marker] in separators else 0: i], re.IGNORECASE):
+        if len(output) == 0 and string[marker] in separators:
+            output += string[marker+1:i]
+        else:
+            output += string[marker:i]
     return output
 
 
 def parse_version(string: str) -> T.Union[Version, None]:
     extended_number_regex = "[0-9]+|\\*"
-    separator = "(?:\\.|-|\\+)"
+    separator = "(?:\\.|-|\\+|:|_)"
     pre_regex = f"(?:{separator}?(alpha|beta|a|b|r|rc){separator}?({extended_number_regex}))"
-    post_regex = f"(?:{separator}?(?:post|p|-|\\.){separator}?({extended_number_regex}))"
+    post_regex = f"(?:{separator}?(?:post|p|{separator}){separator}?({extended_number_regex}))"
     dev_regex = f"(?:{separator}?(?:develop|dev|d){separator}?({extended_number_regex}))"
-    search = re.fullmatch(f'v?(?:({extended_number_regex})(?:!|:))?((?:{extended_number_regex})(?:\\.(?:{extended_number_regex}))*){pre_regex}?{post_regex}?{dev_regex}?', string, re.IGNORECASE)
+    search = re.fullmatch(f'v?(?:(pre|prerelease){separator}?)?(?:({extended_number_regex})(?:!|:|-|_))?((?:{extended_number_regex})(?:\\.(?:{extended_number_regex}))*){pre_regex}?{post_regex}?{dev_regex}?', string, re.IGNORECASE)
     if not search:
         return None
 
-    epoch = search.group(1)
-    release = search.group(2)
-    pre_type = search.group(3)
-    pre_number = search.group(4)
-    post_number = search.group(5)
-    dev_number = search.group(6)
+    epoch = '1'
+    release: T.Tuple[str, ...] = ('0', )
+    pre_type = PreType.NOT_PRE
+    pre_number: T.Union[str, None] = None
+    post_number: T.Union[str, None] = None
+    dev_number: T.Union[str, None] = None
 
-    if epoch is None:
-        epoch = '1'
-    else:
-        epoch = str(epoch)
-    if release is None:
-        release = '0'  # impossible but let's be extra careful
-    else:
-        release = str(release)
-    release = tuple(release.split('.'))
+    match_prerelease = search.group(1)
+    match_epoch = search.group(2)
+    match_release = search.group(3)
+    match_pre_type = search.group(4)
+    match_pre_number = search.group(5)
+    match_post_number = search.group(6)
+    match_dev_number = search.group(7)
 
-    if pre_type is None:
-        pre_type = PreType.NOT_PRE
-    elif pre_type.startswith('a'):
+    if match_prerelease is not None:
         pre_type = PreType.ALPHA
-    elif pre_type.startswith('b'):
-        pre_type = PreType.BETA
-    elif pre_type.startswith('r'):
-        pre_type = PreType.RELEASE_CANDIDATE
-    else:
-        return None  # can not happen
+        pre_number = '1'
 
-    if pre_number is not None:
-        pre_number = str(pre_number)
+    if match_epoch is not None:
+        epoch = str(match_epoch)
 
-    if post_number is not None:
-        post_number = str(post_number)
+    if match_release is not None:
+        release = tuple(str(match_release).split('.'))
 
-    if dev_number is not None:
-        dev_number = str(dev_number)
+    if not match_prerelease or match_pre_type is not None:
+        if match_pre_type is None:
+            pre_type = PreType.NOT_PRE
+        elif match_pre_type.startswith(('a', 'A')):
+            pre_type = PreType.ALPHA
+        elif match_pre_type.startswith(('b', 'B')):
+            pre_type = PreType.BETA
+        elif match_pre_type.startswith(('r', 'R')):
+            pre_type = PreType.RELEASE_CANDIDATE
+        else:
+            return None  # can not happen
+
+        if match_pre_number is not None:
+            pre_number = str(match_pre_number)
+
+    if match_post_number is not None:
+        post_number = str(match_post_number)
+
+    if match_dev_number is not None:
+        dev_number = str(match_dev_number)
 
     return Version(epoch, release, pre_type, pre_number, post_number, dev_number)
