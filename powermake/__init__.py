mactul/powermake

commit 91b59281279d097cbbb3880516e65a5ac5d5ed5f

Improve arg parsing when using action param & test, improve tests

powermake/args_parser.py
@@ -215,7 +215,7 @@ class ArgumentParser(argparse.ArgumentParser):
             description = f"PowerMake {__version__}"
         super().__init__(prog=prog, description=description, **kwargs)
 
-        self.add_argument("action", help='Can be "build", "clean", "install", "test" or "config"', nargs='?')
+        self.add_argument("action", help='Can be "build", "rebuild", "clean", "install", "test" or "config"', nargs='?')
         self.add_argument("install_location", nargs='?', help="Only if the action is set to install, indicate in which folder the installation should be")
         self.add_argument('test_params', nargs='*', help="Only if the action is set to test or if -t (--test) is provided, will be passed to the tested program. You may want to use -- in front of those arguments, like this: python makefile.py -t -- arg1 -option1 arg2 -option2", default=[])
         self.add_argument("--version", help="display PowerMake version", action="store_true")
@@ -268,24 +268,44 @@ def generate_config(target_name: str, args_parsed: T.Union[argparse.Namespace, N
         parser = ArgumentParser()
         args_parsed = parser.parse_args()
 
-    if args_parsed.action is None:
-        pos_args = []
-    else:
+    test_params = []
+    if args_parsed.action is not None:
         if args_parsed.test:
-            pos_args = [args_parsed.action]
-        else:
-            pos_args = []
-        if args_parsed.install_location is not None:
-            pos_args += [args_parsed.install_location] + args_parsed.test_params
+            i = 1
+            action_after = True
+            install_location_after = True
+            while i < len(sys.argv) and sys.argv[i] != '-t' and sys.argv[i] != '--test':
+                if sys.argv[i] == args_parsed.action:
+                    action_after = False
+                if sys.argv[i] == args_parsed.install_location:
+                    install_location_after = False
+                i += 1
+            if action_after:
+                test_params.append(args_parsed.action)
+                args_parsed.action = None
+            if args_parsed.install_location is not None and install_location_after:
+                test_params.append(args_parsed.install_location)
+                args_parsed.install_location = None
+        elif args_parsed.action == "test" and args_parsed.install_location is not None:
+            test_params.append(args_parsed.install_location)
+            args_parsed.install_location = None
 
-    if len(pos_args) > 0 and args_parsed.action != "install" and args_parsed.action != "test" and not args_parsed.test:
-        print(f"Unexpected positional argument {pos_args[0]} with an action different than 'install' and 'test", file=sys.stderr)
+        test_params.extend(args_parsed.test_params)
+
+    if args_parsed.action is not None and args_parsed.action not in {"build", "rebuild", "clean", "install", "test", "config"}:
+        print(f"Unexpected positional argument {args_parsed.action}", file=sys.stderr)
         if parser is not None:
             parser.print_usage(file=sys.stderr)
         exit(1)
 
-    if args_parsed.action == "install" and len(pos_args) > 1:
-        print("install takes a single argument", file=sys.stderr)
+    if args_parsed.install_location is not None and not args_parsed.action == "install":
+        print(f"Unexpected positional argument {args_parsed.install_location}", file=sys.stderr)
+        if parser is not None:
+            parser.print_usage(file=sys.stderr)
+        exit(1)
+
+    if len(test_params) > 0 and args_parsed.action != "test" and not args_parsed.test:
+        print(f"Unexpected positional argument {test_params[0]}", file=sys.stderr)
         if parser is not None:
             parser.print_usage(file=sys.stderr)
         exit(1)
@@ -327,7 +347,9 @@ def generate_config(target_name: str, args_parsed: T.Union[argparse.Namespace, N
     else:
         compilation_unit = uuid.uuid1(random.randint(0, (1 << 48) -1)).hex
 
-    config = Config(target_name, args_parsed=args_parsed, compilation_unit=compilation_unit, verbosity=verbosity, debug=args_parsed.debug, rebuild=args_parsed.rebuild, local_config=args_parsed.local_config, global_config=args_parsed.global_config, operating_system=args_parsed.os, arch=args_parsed.arch, nb_jobs=args_parsed.jobs, single_file=args_parsed.single_file, compile_commands_dir=args_parsed.compile_commands_dir, pos_args=pos_args)
+    rebuild = args_parsed.rebuild or args_parsed.action == "rebuild"
+
+    config = Config(target_name, args_parsed=args_parsed, compilation_unit=compilation_unit, verbosity=verbosity, debug=args_parsed.debug, rebuild=rebuild, local_config=args_parsed.local_config, global_config=args_parsed.global_config, operating_system=args_parsed.os, arch=args_parsed.arch, nb_jobs=args_parsed.jobs, single_file=args_parsed.single_file, compile_commands_dir=args_parsed.compile_commands_dir, test_params=test_params)
 
     config.add_flags(*config.get_cmdline_additional_flags())
 
@@ -379,7 +401,7 @@ def run_callbacks(config: Config, *, build_callback: T.Callable[[Config], None],
 
     if config._args_parsed.action == "clean" or config._args_parsed.clean:
         clean = True
-    if config._args_parsed.action == "build" or config._args_parsed.build or config._args_parsed.rebuild:
+    if config._args_parsed.action == "build" or config._args_parsed.action == "rebuild" or config._args_parsed.build or config._args_parsed.rebuild:
         build = True
     if config._args_parsed.action == "install" or config._args_parsed.install is not False or config._args_parsed.i:
         install = True
@@ -399,7 +421,7 @@ def run_callbacks(config: Config, *, build_callback: T.Callable[[Config], None],
             else:
                 install_callback(config, config._args_parsed.install or None)
         if test:
-            test_callback(config, config._pos_args)
+            test_callback(config, config._test_params)
     except PowerMakeException as e:
         if config.compile_commands_dir is not None:
             # try to update the compile_commands.json file as much as possible

tests/tests.sh
@@ -12,9 +12,9 @@ cd "$(dirname "$0")"
 
 export PYTHONPATH=$PYTHONPATH:$(pwd)/../
 
-python3 ./multiplatform/makefile.py --delete-cache
+coverage run ./multiplatform/makefile.py --delete-cache
 
-coverage run ./units/tests_main.py || failure
+coverage run -a ./units/tests_main.py || failure
 
 # coverage report && coverage html && firefox htmlcov/index.html
 # exit 0
@@ -101,6 +101,46 @@ if [ ! -f "./multiplatform/install/bin/test" ]
 then
     failure
 fi
+
+coverage run -a ./multiplatform/makefile.py -bvt | grep "Hello" || failure
+
+coverage run -a ./multiplatform/makefile.py test | grep "Hello" || failure
+
+coverage run -a ./multiplatform/makefile.py clean || failure
+coverage run -a ./multiplatform/makefile.py test | grep "Nothing to run" || failure
+
+coverage run -a ./multiplatform/makefile.py build -t | grep "Hello" || failure
+coverage run -a ./multiplatform/makefile.py build -t | grep "build" && failure
+coverage run -a ./multiplatform/makefile.py clean || failure
+coverage run -a ./multiplatform/makefile.py -t build | grep "Nothing to run" || failure
+coverage run -a ./multiplatform/makefile.py build -t foo bar | grep "build" && failure
+coverage run -a ./multiplatform/makefile.py rebuild -t foo bar | grep -Pzo 'foo\nbar' || failure
+coverage run -a ./multiplatform/makefile.py -t build | grep "build" || failure
+rm -rf ./multiplatform/install
+coverage run -a ./multiplatform/makefile.py install ./install -t build | grep "build" || failure
+if [ ! -f "./multiplatform/install/bin/test" ]
+then
+    failure
+fi
+rm -rf ./multiplatform/install
+coverage run -a ./multiplatform/makefile.py --test install ./install build | grep -Pzo 'install\n./install\nbuild' || failure
+if [ -f "./multiplatform/install/bin/test" ]
+then
+    failure
+fi
+
+coverage run -a ./multiplatform/makefile.py foo bar something 2>&1 | grep "Unexpected positional argument foo" || failure
+
+coverage run -a ./multiplatform/makefile.py build bar something 2>&1 | grep "Unexpected positional argument bar" || failure
+
+coverage run -a ./multiplatform/makefile.py install ./install something 2>&1 | grep "Unexpected positional argument something" || failure
+
+coverage run -a ./multiplatform/makefile.py test foo bar 2>&1 | grep -Pzo 'foo\nbar' || failure
+coverage run -a ./multiplatform/makefile.py test foo bar 2>&1 | grep -E "^test$" && failure
+
+coverage run -a ./multiplatform/makefile.py -qv 2>&1 | grep "Passing --quiet and --verbose arguments in the same time doesn't make any sense." || failure
+
+coverage run -a ./multiplatform/makefile.py --version | grep "Apache-2.0" || failure
 
 coverage report
 

coverage.svg
@@ -1,6 +1,6 @@
 
-<svg width="96.3" height="20" viewBox="0 0 963 200" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="coverage: 85%">
-<title>coverage: 85%</title>
+<svg width="96.3" height="20" viewBox="0 0 963 200" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="coverage: 86%">
+<title>coverage: 86%</title>
 <linearGradient id="zxDZo" x2="0" y2="100%">
     <stop offset="0" stop-opacity=".1" stop-color="#EEE"/>
     <stop offset="1" stop-opacity=".1"/>
@@ -8,13 +8,13 @@
 <mask id="zPoWN"><rect width="963" height="200" rx="30" fill="#FFF"/></mask>
 <g mask="url(#zPoWN)">
     <rect width="603" height="200" fill="#555"/>
-    <rect width="360" height="200" fill="hsl(88.8, 100%, 40%)" x="603"/>
+    <rect width="360" height="200" fill="hsl(90.9, 100%, 40%)" x="603"/>
     <rect width="963" height="200" fill="url(#zxDZo)"/>
 </g>
 <g aria-hidden="true" fill="#fff" text-anchor="start" font-family="Verdana,DejaVu Sans,sans-serif" font-size="110">
     <text x="60" y="148" textLength="503" fill="#000" opacity="0.25">coverage</text>
     <text x="50" y="138" textLength="503">coverage</text>
-    <text x="658" y="148" textLength="260" fill="#000" opacity="0.25">85%</text>
-    <text x="648" y="138" textLength="260">85%</text>
+    <text x="658" y="148" textLength="260" fill="#000" opacity="0.25">86%</text>
+    <text x="648" y="138" textLength="260">86%</text>
 </g>
 </svg>

powermake/config.py
@@ -366,7 +366,7 @@ def replace_architecture(string: str, new_arch: str) -> str:
 
 
 class Config:
-    def __init__(self, target_name: str, *, args_parsed: argparse.Namespace, compilation_unit: str, debug: bool = False, rebuild: bool = False, verbosity: int = 1, nb_jobs: int = 0, operating_system: T.Union[str, None] = None, arch: T.Union[str, None] = None, single_file: T.Union[str, None] = None, compile_commands_dir: T.Union[str, None] = None, local_config: T.Union[str, None] = "./powermake_config.json", global_config: T.Union[str, None] = None, pos_args: T.List[str] = []) -> None:
+    def __init__(self, target_name: str, *, args_parsed: argparse.Namespace, compilation_unit: str, debug: bool = False, rebuild: bool = False, verbosity: int = 1, nb_jobs: int = 0, operating_system: T.Union[str, None] = None, arch: T.Union[str, None] = None, single_file: T.Union[str, None] = None, compile_commands_dir: T.Union[str, None] = None, local_config: T.Union[str, None] = "./powermake_config.json", global_config: T.Union[str, None] = None, test_params: T.List[str] = []) -> None:
         """
         Create an object that loads all configurations files and search for compilers.
 
@@ -376,7 +376,7 @@ class Config:
         """
         self.target_name = target_name
         self._args_parsed = args_parsed
-        self._pos_args = pos_args
+        self._test_params = test_params
         self.verbosity = verbosity
         self.debug = debug
         self.rebuild = rebuild

tests/multiplatform/main.cpp
@@ -4,8 +4,12 @@ extern "C" {
     void sleep_ms(unsigned int ms);
 }
 
-int main()
+int main(int argc, char* argv[])
 {
+    for(int i = 1; i < argc; i++)
+    {
+        std::cout << argv[i] << std::endl;
+    }
     std::cout << "Hello" << std::endl;
     sleep_ms(10);
     std::cout << "World" << std::endl;

commit b29f5de8cb695512645ce3760145ac305c47fb8d

Fix a regression introduced by 91b59281

powermake/__init__.py
@@ -87,7 +87,7 @@ __all__ = [
     "print_debug_info",
     "warning_text",
     "PowerMakeRuntimeError",
-    "PowermakeValueError",
+    "PowerMakeValueError",
     "Operation",
     "needs_update",
     "run_command",

powermake/args_parser.py
@@ -274,7 +274,7 @@ def generate_config(target_name: str, args_parsed: T.Union[argparse.Namespace, N
             i = 1
             action_after = True
             install_location_after = True
-            while i < len(sys.argv) and sys.argv[i] != '-t' and sys.argv[i] != '--test':
+            while i < len(sys.argv) and (sys.argv[i][0] != '-' or 't' not in sys.argv[i]) and sys.argv[i] != '--test':
                 if sys.argv[i] == args_parsed.action:
                     action_after = False
                 if sys.argv[i] == args_parsed.install_location:

tests/tests.sh
@@ -137,6 +137,9 @@ coverage run -a ./multiplatform/makefile.py install ./install something 2>&1 | g
 
 coverage run -a ./multiplatform/makefile.py test foo bar 2>&1 | grep -Pzo 'foo\nbar' || failure
 coverage run -a ./multiplatform/makefile.py test foo bar 2>&1 | grep -E "^test$" && failure
+coverage run -a ./multiplatform/makefile.py -rt foo bar 2>&1 | grep -Pzo 'foo\nbar' || failure
+coverage run -a ./multiplatform/makefile.py -rt foo bar 2>&1 | grep -E "^test$" && failure
+
 
 coverage run -a ./multiplatform/makefile.py -qv 2>&1 | grep "Passing --quiet and --verbose arguments in the same time doesn't make any sense." || failure
 